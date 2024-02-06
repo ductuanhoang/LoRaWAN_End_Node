835390445cb740af1e9225095a5c9a2cb46b6452
ductuan <ductuanhoang@users.noreply.github.com>
2024-02-06 11:49:04 +0700

    Initial commit

---
+++ STM32CubeIDE/Debug/Utilities/stm32_adv_trace.d
@@ -0,0 +1,16 @@
+Utilities/stm32_adv_trace.o: \
+ D:/2.upwork/83.Lora_water_leak/Example_code/en.stm32cubewl-v1-3-0/STM32Cube_FW_WL_V1.3.0/Utilities/trace/adv_trace/stm32_adv_trace.c \
+ D:/2.upwork/83.Lora_water_leak/Example_code/en.stm32cubewl-v1-3-0/STM32Cube_FW_WL_V1.3.0/Utilities/trace/adv_trace/stm32_adv_trace.h \
+ ../../Core/Inc/utilities_conf.h \
+ ../../../../../../../Drivers/CMSIS/Include/cmsis_compiler.h \
+ ../../../../../../../Drivers/CMSIS/Include/cmsis_gcc.h \
+ ../../../../../../../Utilities/misc/stm32_mem.h \
+ ../../../../../../../Utilities/misc/stm32_tiny_vsnprintf.h \
+ ../../Core/Inc/utilities_def.h
+D:/2.upwork/83.Lora_water_leak/Example_code/en.stm32cubewl-v1-3-0/STM32Cube_FW_WL_V1.3.0/Utilities/trace/adv_trace/stm32_adv_trace.h:
+../../Core/Inc/utilities_conf.h:
+../../../../../../../Drivers/CMSIS/Include/cmsis_compiler.h:
+../../../../../../../Drivers/CMSIS/Include/cmsis_gcc.h:
+../../../../../../../Utilities/misc/stm32_mem.h:
+../../../../../../../Utilities/misc/stm32_tiny_vsnprintf.h:
+../../Core/Inc/utilities_def.h:
---
+++ STM32CubeIDE/Debug/Utilities/stm32_tiny_vsnprintf.d
@@ -0,0 +1,4 @@
+Utilities/stm32_tiny_vsnprintf.o: \
+ D:/2.upwork/83.Lora_water_leak/Example_code/en.stm32cubewl-v1-3-0/STM32Cube_FW_WL_V1.3.0/Utilities/misc/stm32_tiny_vsnprintf.c \
+ D:/2.upwork/83.Lora_water_leak/Example_code/en.stm32cubewl-v1-3-0/STM32Cube_FW_WL_V1.3.0/Utilities/misc/stm32_tiny_vsnprintf.h
+D:/2.upwork/83.Lora_water_leak/Example_code/en.stm32cubewl-v1-3-0/STM32Cube_FW_WL_V1.3.0/Utilities/misc/stm32_tiny_vsnprintf.h:
